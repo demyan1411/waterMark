40f61ac6d109892836529dd8b3b642d3f073547b
Andrey Balandin <vinc@fromru.com>
2015-09-05 13:41:56 +0500

    js-translate: Установка обработчиков событий на кнопки выбора языка

--- app/js/translate.js
+++ app/js/translate.js
@@ -1,29 +1,36 @@
 /* translate.js
    модуль устанавливает обработчик события на кнопки выбора языка
-   и содержит функцию для изменения текста элементов с классом ".text"
+   и содержит функцию для изменения текста элементов с атрибутом 'data-lang'
+
+   Пример:
+   Чтобы изменить язык на английский, набрать в консоли: translate.changeLang(1)
 */
 'use strict';
 
 var translate = (function() {
 
+  // элементы с атрибутом 'data-lang', у которых будем менять текст
+  var elemsWithLang = [];
+
   // инициализация
   var init = function () {
+    elemsWithLang = $('[data-lang]');
     _setupListeners();
   };
 
   // установка обработчиков событий
   var _setupListeners = function() {
-    //$('.settings__btn-download').on('click', _makePicture);
+    $('.icons__language-link').on('click', function(event) {
+      event.preventDefault();
+      changeLang(this.getAttribute("href"));
+    });
   };
 
-  // изменить текст элементов с атрибутом 'lang'
-  var changeLang = function() {
-
-    //event.preventDefault();
-
-    $('[data-lang]').each(function() {
+  // изменить текст элементов с атрибутом 'data-lang'
+  var changeLang = function(numLang) {
+    elemsWithLang.each(function() {
       var $this = $(this);
-      $this.text( $this.data('lang')[1] );
+      $this.text( $this.data('lang')[numLang] );
     });
   };
 
@@ -34,4 +41,6 @@ var translate = (function() {
 
 })();
 
-translate.init();
+$(document).ready(function() {
+  translate.init();
+});
