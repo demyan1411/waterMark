9bf71017bd7a486eea6835cce85f92d6fd3e12c1
unknown <demian.drozdov@d-drozdov.corp.dailyprofit.ru>
2015-09-10 10:47:32 +0300

    исправил переменные

--- app/js/imgsubmit.js
+++ app/js/imgsubmit.js
@@ -23,12 +23,15 @@ var picModule = (function() {
 		if(app.watermark.mode === 'tile') {
 			app.watermark.coordx = parseInt($('.repeatBlock').css('left').slice(0, -2), 10);
 			app.watermark.coordy = parseInt($('.repeatBlock').css('top').slice(0, -2), 10);
-			app.watermark.marginx = $('.posX').val();
-			app.watermark.marginy = $('.posY').val();
+			app.watermark.marginx = parseInt($('.posX').val(), 10);
+			app.watermark.marginy = parseInt($('.posY').val(), 10);
 		} else {
-			app.watermark.coordx = $('.posX').val();
-			app.watermark.coordy = $('.posY').val();
+			app.watermark.coordx = parseInt($('.posX').val(), 10);
+			app.watermark.coordy = parseInt($('.posY').val(), 10);
 		}
+		console.log(app.watermark.coordx);
+		console.log(app.watermark.coordy);
+		console.log(app.watermark.opacity);
 
 		$.ajax({
 				url: app.URL_WATERMARK_REQUEST,
